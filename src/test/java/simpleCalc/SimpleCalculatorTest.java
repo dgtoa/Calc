ff0a0c7b5077fdcb84ef15f37d266d093867507f
Bokyung Seo <qhrud8015@naver.com>
2016-04-11 16:34:39 +0900

    뺄셈기능과 테스트코드 추가
    기본적 기능

--- src/test/java/simpleCalc/SimpleCalculatorTest.java
+++ src/test/java/simpleCalc/SimpleCalculatorTest.java
@@ -2,7 +2,7 @@ package simpleCalc;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.Test;                        //이거 두개 추가시켜요
 
 public class SimpleCalculatorTest {
 
@@ -12,6 +12,14 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc= new SimpleCalculator();
 		calc.add(10,20);
 		assertEquals(30, calc.getResult());
+		//	fail("Not yet implemented");
+	}
+	
+	public void testSub() 
+	{
+		SimpleCalculator calc= new SimpleCalculator();
+		calc.sub(10,20);
+		assertEquals(-10, calc.getResult());
 		//	fail("Not yet implemented");
 	}
 
